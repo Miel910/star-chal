54a82ad0f0c3bfa780b008a39e3a5590a31a5b23
Miel910 <111350781+Miel910@users.noreply.github.com>
2023-05-01 19:32:45 +0300

    Update star-chal.dart

--- star-chal.dart
+++ star-chal.dart
@@ -1,14 +1,11 @@
 void main() {
+  
   var star = "*";
-  var s = 3;
-
-  for (int i = 0; i < 9; i++) {
-    if (i < 4) {
-      print(star);
-      star += "*";
-    } else if (s > 0) {
-      print(star.substring(star.length - s));
-      s--;
-    }
+  int s = 3;
+  
+  for (int i = 0; i < 9; i++){    
+    if(i < 4){print(star); star += "*";}
+    else if(s > 0){print(star.substring(star.length -s)); s--;}
+    
   }
 }
