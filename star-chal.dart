e022edd1a69ad107b83cb111f7a866e5fdda8667
Abdulmohsen Alfreh <abdul-muhsen@hotmail.com>
2023-05-02 11:33:27 +0300

    delete lines

--- star-chal.dart
+++ star-chal.dart
@@ -1,14 +1,10 @@
 void main() {
   var star = "*";
-  var s = 3;
 
   for (int i = 0; i < 9; i++) {
     if (i < 4) {
       print(star);
       star += "*";
-    } else if (s > 0) {
-      print(star.substring(star.length - s));
-      s--;
     }
   }
 }
