1e883ec96096ea770766c4895070881cd8c2ba7e
Abdulmohsen Alfreh <abdul-muhsen@hotmail.com>
2023-05-02 11:37:03 +0300

    comments

--- star-chal.dart
+++ star-chal.dart
@@ -1,10 +1,14 @@
 void main() {
   var star = "*";
+  // int s = 3;
 
   for (int i = 0; i < 9; i++) {
     if (i < 4) {
       print(star);
       star += "*";
-    }
+    } /*else if (s > 0) {
+      print(star.substring(star.length - s));
+      s--;
+    }*/
   }
 }
